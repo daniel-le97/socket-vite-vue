3a4fa3c2601a563f6c6a81687c3ebf6b0bdc47e0
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-04-03 14:49:57 -0600

    move all sqlite code to extensions

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-// import { db } from './services/sqlite'
+import { db } from './services/sqlite'
 
-// await db.exec('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);')
+await db.exec('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);')
 
-// for (let index = 0; index < 1000; index++) {
-//   const res = await db.exec(`INSERT INTO test (name) VALUES ('test ${index}');`)
-//   if (res?.err)
-//     console.log('Failed to insert', res.err)
-// }
+for (let index = 0; index < 1000; index++) {
+  const res = await db.exec(`INSERT INTO test (name) VALUES ('test ${index}');`)
+  if (res?.err){
+
+      // eslint-disable-next-line no-console
+      console.log('Failed to insert', res.err)
+    }
+}
 
 createApp(App).mount('#app')
